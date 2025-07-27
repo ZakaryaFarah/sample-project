74f1ae4696acdc49953e44035928eb4b9caebf79
Zakarya Farah <Farah41@email.franklin.edu>
2025-07-27 14:46:28 -0400

    Update soccer.js

--- soccer.js
+++ soccer.js
@@ -1,5 +1,10 @@
-const players = ["Ronaldo", "Messi", "Ibra", "Neymar"];
+try {
+    const players = ["Ronaldo", "Messi", "Ibra", "Neymar"];
 
 players.forEach((player, index) => {
     console.log(`${index + 1}. ${player}`);
 });
+} catch (err) {
+    console.error(" Error: " + err.message);
+    process.exit(1);
+}
